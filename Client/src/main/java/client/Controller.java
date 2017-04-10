b8f9fd4fac5cc8b3e81d0ead522a3c19a0350076
Vagik <simonyan.vagik@yandex.ru>
2017-04-10 14:02:29 +0300

    avoid conflicts

--- Client/src/main/java/client/Controller.java
+++ Client/src/main/java/client/Controller.java
@@ -17,32 +17,32 @@ public class Controller {
     public ImageView testCar3;
     public ImageView testCar4;
 
-    public ImageView police1,police2;
-    public ImageView police3,police4;
-    public ImageView police5,police6;
-    public ImageView police7,police8;
+    public ImageView police1, police2;
+    public ImageView police3, police4;
+    public ImageView police5, police6;
+    public ImageView police7, police8;
 
-    public int PlayersCount = 1;
+    public int PlayersCount = 2;
 
-    Player[]player = new Player[PlayersCount];
-    String []PlayersNames = new String[PlayersCount];
-    ImageView []images = new ImageView[4];
+    Player[] player = new Player[PlayersCount];
+    String[] PlayersNames = new String[PlayersCount];
+    ImageView[] images = new ImageView[4];
 
     public void begin(ActionEvent actionEvent) {
 
-        for(int i=0; i<PlayersCount; i++) {
+        for (int i = 0; i < PlayersCount; i++) {
             PlayersNames[i] = "Player" + i;
 
         }
 
-        images[0]=testCar;
-        images[1]=testCar2;
-        images[2]=testCar3;
-        images[3]=testCar4;
+        images[0] = testCar;
+        images[1] = testCar2;
+        images[2] = testCar3;
+        images[3] = testCar4;
 
-        for(int i=0; i<PlayersCount; i++) {
-            player[i] = new Player(images[i],police1,police2,police3,police4,police5,police6,police7,police8,
-                                   PlayersNames[i], pain2, pain3, mainPane,PlayersCount, i);
+        for (int i = 0; i < PlayersCount; i++) {
+            player[i] = new Player(images[i], police1, police2, police3, police4, police5, police6, police7, police8,
+                    PlayersNames[i], pain2, pain3, mainPane, PlayersCount, i);
             player[i].start();
         }
     }
